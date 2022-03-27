6aabfc74a3c0672ac4cda8fb14150ff89284e383
aerinp <aerin_park@sfu.ca>
2022-03-27 05:43:05 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,53 +6,6 @@
 #include "pcb.h"
 #include "list.h"
 
-void commands(){
-    char command;
-    switch(command){
-        case 'C':
-        break;
-
-        case 'F':
-        break;
-
-        case 'K':
-        break;
-
-        case 'E':
-        break;
-
-        case 'Q':
-        break;
-
-        case 'S':
-        break;
-
-        case 'R':
-        break;
-
-        case 'Y':
-        break;
-
-        case 'N':
-        break;
-
-        case 'P':
-        break;
-
-        case 'V':
-        break;
-
-        case 'I':
-        break;
-
-        case 'T':
-        break;
-
-        // default:
-
-    }
-}
-
 void pcb_command_list(){
     printf("C: Create\n");
     printf("F: Fork\n");
